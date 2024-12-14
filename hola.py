b5ac126983eb837b7e439700adc05cd838165966
Felipe <felipe.nago898@gmail.com>
2024-12-14 10:31:39 -0500

    Add index.html and index.css and hola.py

---
+++ hola.py
@@ -0,0 +1,13 @@
+class Avion:
+
+    def __init__ (self, user, email, id):
+
+        self.__user = user
+        self.__email = email
+        self.__id = id
+
+    def GetUser (self):
+        return self.__user
+    
+
+    
